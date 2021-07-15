7c83b8845033c7483de20e10c01f168db29c2b25
shodgetts <stephenhodgetts91@gmail.com>
2021-07-15 17:26:56 -0600

    4

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const port = process.env.PORT
 const base64 = require("js-base64").Base64;
 const fetch = require("node-fetch");
 
-//chat client
+//chat 
 const StreamChat = require("stream-chat").StreamChat;
 const chatClient = StreamChat.getInstance('qtc55pny5xww', '9hrptm6edfg8ygm6hzswpj8rnze94v8ujfmg7drfk8ndaztnqkh875upke6ndypc');
+const user_id = 'throbbing-river-1'
+const type = 'messagiing'
+const id = 'first'
 
 // node cron
 const cron = require('node-cron')
@@ -25,9 +28,10 @@ app.get('/', (req, res) => {
 
 cron.schedule('* * * * *', () => {
   console.log('running every minute');
+  const channel = chatClient.channel(type, id)
+  channel.sendMessage({ text: 'does this work?', user_id })
 });
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
-
