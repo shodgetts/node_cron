589b128c2c6a2f499f92ee1ffa0a1b5b180240f4
shodgetts <stephenhodgetts91@gmail.com>
2021-07-15 21:24:05 -0600

    issue with port

--- src/server.js
+++ src/server.js
@@ -45,6 +45,6 @@ cron.schedule('* 30 * * * *', () => {
   console.log('running every 30 minutes');
 });
 
-app.listen(() => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`server running on port 5000`);
 });
