c59c49badea0fcea51297c053967e632f1da80b3
shodgetts <stephenhodgetts91@gmail.com>
2021-07-16 07:55:56 -0600

    remove token

--- src/services/generateStreamChatClient.js
+++ src/services/generateStreamChatClient.js
@@ -1,4 +1,4 @@
 const StreamChat = require("stream-chat").StreamChat;
-const client = new StreamChat('dz5f4d5kzrue', 'msp77tpbfcmmv98pk598jvvu93rygd3nexctp7c74tgyfsfd3ub8d9dg9gtz6a6s', { allowServerSideConnect: true });
+const client = new StreamChat(API_KEY, API_SECRET, { allowServerSideConnect: true });
 
 exports.client = client;
